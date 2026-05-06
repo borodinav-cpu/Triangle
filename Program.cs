020186057ba515e3602237ccaddc01397f9a962c
sinica gestyanka <borodinav@mer.ci.nsu.ru>
2026-05-06 20:39:18 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите A: ");
+        string a = Console.ReadLine();
+
+        Console.Write("Введите B: ");
+        string b = Console.ReadLine();
+
+        Console.Write("Введите C: ");
+        string c = Console.ReadLine();
+
+        var result = TriangleService.Calculate(a, b, c);
+
+        Console.WriteLine($"Тип треугольника: {result.Item1}");
+        Console.WriteLine("Координаты:");
+
+        foreach (var point in result.Item2)
+        {
+            Console.WriteLine(point);
+        }
+
+        Log(a, b, c, result);
+    }
+
+    static void Log(string a, string b, string c, (string, List<(int, int)>) result)
+    {
+        string time = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        string parameters = $"A={a}, B={b}, C={c}";
+        string coordinates = string.Join(", ", result.Item2);
+
+        bool success = result.Item1 == "равносторонний" ||
+                       result.Item1 == "равнобедренный" ||
+                       result.Item1 == "разносторонний";
+
+        string logMessage;
+
+        if (success)
+        {
+            logMessage = $"{time} | SUCCESS | {parameters} | Тип: {result.Item1} | Коорд: {coordinates}";
+        }
+        else
+        {
+            logMessage = $"{time} | ERROR | {parameters} | Результат: {result.Item1} | Коорд: {coordinates}";
+        }
+
+        // вывод в консоль
+        Console.WriteLine("LOG: " + logMessage);
+
+        // запись в файл
+        File.AppendAllText("app.log", logMessage + Environment.NewLine);
+    }
+}
---
+++ TriangleService.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class TriangleService
+{
+    public static (string, List<(int, int)>) Calculate(string aStr, string bStr, string cStr)
+    {
+        if (!float.TryParse(aStr, out float a) ||
+            !float.TryParse(bStr, out float b) ||
+            !float.TryParse(cStr, out float c))
+        {
+            return ("", new List<(int, int)> { (-2, -2), (-2, -2), (-2, -2) });
+        }
+
+        if (a <= 0 || b <= 0 || c <= 0)
+        {
+            return ("", new List<(int, int)> { (-2, -2), (-2, -2), (-2, -2) });
+        }
+
+        if (a + b <= c || a + c <= b || b + c <= a)
+        {
+            return ("не треугольник", new List<(int, int)> { (-1, -1), (-1, -1), (-1, -1) });
+        }
+
+        string type;
+
+        if (a == b && b == c)
+            type = "равносторонний";
+        else if (a == b || a == c || b == c)
+            type = "равнобедренный";
+        else
+            type = "разносторонний";
+
+        var coords = GetCoordinates(a, b, c);
+
+        return (type, coords);
+    }
+
+    private static List<(int, int)> GetCoordinates(float a, float b, float c)
+    {
+        double x = (b * b + c * c - a * a) / (2 * c);
+        double y = Math.Sqrt(Math.Max(b * b - x * x, 0));
+
+        var points = new List<(double, double)>
+        {
+            (0, 0),
+            (c, 0),
+            (x, y)
+        };
+
+        double maxX = double.MinValue, maxY = double.MinValue;
+
+        foreach (var p in points)
+        {
+            if (p.Item1 > maxX) maxX = p.Item1;
+            if (p.Item2 > maxY) maxY = p.Item2;
+        }
+
+        double scale = 90 / Math.Max(maxX, maxY);
+
+        var result = new List<(int, int)>();
+
+        foreach (var p in points)
+        {
+            int sx = (int)(p.Item1 * scale + 5);
+            int sy = (int)(p.Item2 * scale + 5);
+            result.Add((sx, sy));
+        }
+
+        return result;
+    }
+}
